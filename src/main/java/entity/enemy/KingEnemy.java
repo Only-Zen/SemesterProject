4fa762469bb507b99ed87e2b0814ddd1de1f7c82
Only-Zen <jackson.m.carbone@gmail.com>
2025-04-22 15:20:08 -0500

    Updated Build Method Modified King Rat Stats

--- src/main/java/entity/enemy/KingEnemy.java
+++ src/main/java/entity/enemy/KingEnemy.java
@@ -9,7 +9,7 @@ import main.GamePanel;
 public class KingEnemy extends Enemy {
 
     public KingEnemy(Coordinate position, GamePanel gp){
-        this ("King", position, 1, 12500, 100, 100, gp);
+        this ("King", position, 1, 14000, 100, 100, gp);
     }
     public KingEnemy(String name, Coordinate position, int speed, int health, int damage, int value, GamePanel gp){
         super(name, position, speed, health, damage, value, gp);
